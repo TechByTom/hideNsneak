42a63eb7fcf38e574998146dc3938e204f50fcaf
Mike Hodges <rmikehodges@gmail.com>
2018-04-08 13:36:54 -0400

    moved template files to constants

--- google/cloudRedirector.go
+++ google/cloudRedirector.go
@@ -21,8 +21,89 @@ type RedirectorSource struct {
 	C2Url            string
 }
 
+const appYaml = `runtime: go
+api_version: go1
+
+handlers:
+- url: /.*
+  script: _go_app`
+
+const redirectorString = `package goengine
+import (
+	"net"
+	"net/http"
+	"net/url"
+
+	"github.com/rmikehodges/gaereverseproxy"
+)
+
+type Prox struct {
+	// target url of reverse proxy
+	target *url.URL
+	// instance of Go ReverseProxy thatwill do the job for us
+	proxy *gaereverseproxy.ReverseProxy
+}
+
+func validUA(userAgent string) bool {
+	ua := "{{.RestrictedUA}}"
+	if ua != "" && ua != userAgent {
+		return false
+	}
+	return true
+}
+
+func validIP(remoteIP string) bool {
+	subnet := "{{.RestrictedSubnet}}"
+	if len(subnet) > 8 {
+		_, cidr, err := net.ParseCIDR(subnet)
+		if err != nil {
+			return false
+		}
+		if !cidr.Contains(net.ParseIP(remoteIP)) {
+			return false
+		}
+	}
+	return true
+}
+
+//TODO: Add header templating
+func validHeader(remoteHeader http.Header) bool {
+	header, value := "{{.HeaderName}}", "{{.HeaderValue}}"
+	if header != "" && value != "" {
+		if remoteHeader.Get(header) != value {
+			return false
+		}
+	}
+	return true
+}
+
+// small factory
+func New(target string) *Prox {
+	url, _ := url.Parse(target)
+	// you should handle error on parsing
+	return &Prox{target: url, proxy: gaereverseproxy.NewSingleHostReverseProxy(url)}
+}
+
+func (p *Prox) handle(w http.ResponseWriter, r *http.Request) {
+	// call to magic method from ReverseProxy object
+	if !validUA(r.UserAgent()) || !validIP(r.RemoteAddr) || !validHeader(r.Header) {
+		http.Redirect(w, r, "{{.DefaultRedirect}}", 301)
+	} else {
+		p.proxy.ServeHTTP(w, r)
+	}
+}
+
+func init() {
+	proxy := New("{{.C2Url}}")
+
+	// server
+	http.HandleFunc("/", proxy.handle)
+}
+
+`
+
 func generateSource(projectDir string, redirector RedirectorSource) bool {
-	redirectorTemplate, err := template.ParseFiles(projectDir + "/redirector.tpl")
+	redirectorTemplate, err := template.ParseGlob(redirectorString)
 	if err != nil {
 		//Log here
 		log.Println("Error Parsing template file")
@@ -41,12 +122,25 @@ func generateSource(projectDir string, redirector RedirectorSource) bool {
 		log.Println("Error creating Go file")
 		return false
 	}
+	defer outFile.Close()
 	err = redirectorTemplate.Execute(outFile, redirector)
 	if err != nil {
 		//Log here
 		log.Printf("Error marshalling redirector object to template file: %s", err)
 		return false
 	}
+	appFile, err := os.Create(projectDir + "/app.yaml")
+	if err != nil {
+		log.Println("Error creating app.yaml")
+		return false
+	}
+	defer appFile.Close()
+	_, err = appFile.Write([]byte(appYaml))
+	if err != nil {
+		log.Println("Error writing app.yaml")
+		return false
+	}
+
 	return true
 }
 
